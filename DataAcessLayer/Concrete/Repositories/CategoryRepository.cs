85bc505b975e844fb2073f683a9e942e058aa13d
ByFatiq <45851501+ByFatiq@users.noreply.github.com>
2021-05-05 18:36:06 +0300

    22. End of the lesson. Bugs have been fixed.

--- DataAcessLayer/Concrete/Repositories/CategoryRepository.cs
+++ DataAcessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -2,37 +2,45 @@
 using System.Collections.Generic;
  using System.Data.Entity;
  using System.Linq;
-using System.Text;
+ using System.Linq.Expressions;
+ using System.Text;
 using System.Threading.Tasks;
  using DataAcessLayer.Abstract;
 using EntityLayer.Concrete;
 
 namespace DataAcessLayer.Concrete.Repositories
 {
-    public class CategoryRepository :IRepository<>
+    public class CategoryRepository :ICategoryDal
     {
-       Context C = new Context();
-      DbSet<T> _object;
+       Context c = new Context();
+      DbSet<Category> _object;
 
-        public void Delete(T p)
-        {
-            return _object.Remove();
-            
-        }
+      
 
-        public void Insert(T p)
-        {
-            return _object.Add(p);
-            p.Sa
-            
-        }
-
-        public List<T> List()
+        public List<Category> List()
         {
             return _object.ToList();
         }
 
-        public void Update(T p)
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+
+        }
+
+        public void Update(Category p)
+        {
+          
+        }
+
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
         {
             throw new NotImplementedException();
         }
--- MvcProjectCamp/Controllers/CategoryController.cs
+++ MvcProjectCamp/Controllers/CategoryController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer.Concrete;
 
 namespace MvcProjectCamp.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager();
+
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL();
+            return View(categoryvalues);
         }
     }
 }
