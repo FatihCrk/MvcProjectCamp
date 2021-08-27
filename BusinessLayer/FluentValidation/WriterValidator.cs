e3232d2bbd7c0a59119413268cbe11d593dcb4b1
ByFatiq <45851501+ByFatiq@users.noreply.github.com>
2021-08-27 15:36:50 +0300

    WriterValidator Sorunlar giderildi.

--- BusinessLayer/FluentValidation/WriterValidator.cs
+++ BusinessLayer/FluentValidation/WriterValidator.cs
@@ -14,13 +14,17 @@ namespace BusinessLayer.FluentValidation
 
         public WriterValidator()
         {
-            RuleFor(x => x.WriterId).NotEmpty().WithMessage("Yazar Id boş geçilemez.");
+           
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar ismi boş geçilemez.");
+
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Yazarın adı en az 2 karakter olmalıdır.");
+
             RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazarın soyadı boş geçilemez.");
-            RuleFor(x => x.WriterSurName).MinimumLength(2).WithMessage("Yazar soyadı 2 karakterden uzun olmalıdır.");
-            RuleFor(x => x.WriterSurName).MaximumLength(20).WithMessage("Yazay soyadı 20 karakterden uzun olmamalı.");
-           // RuleFor(x => x.WriterName).Custom(harfBul("a").WithMessage($"Yazar ismi 'a' harfi içermelidir.");
+
+            RuleFor(x => x.WriterSurName).MinimumLength(2).WithMessage("Yazar soyadı en az 2 karakter olmalıdır.");
+
+            RuleFor(x => x.WriterSurName).MaximumLength(20).WithMessage("Yazay soyadı en fazla 20 karakter olmalı.");
+
             RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Yazar unvanı boş geçilemez.");
 
         }
