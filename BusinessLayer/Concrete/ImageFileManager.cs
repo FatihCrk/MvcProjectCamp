694df9faebdd0537a5eb0ad806f022a63a18be9d
ByFatiq <45851501+ByFatiq@users.noreply.github.com>
2021-09-14 00:04:52 +0300

    68. Ders

---
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class ImageFileManager:IImageFileService
+    {
+        private IImageFileDal _imageFileDal;
+
+        public ImageFileManager(IImageDal contentDal)
+        {
+            _imageFileDal = contentDal;
+        }
+    }
+}
