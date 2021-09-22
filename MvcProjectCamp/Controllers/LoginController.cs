e076747864c2b53b8e3bae86c084ca157b5f8b3c
Fatih ÇIRAK <fatihcirak@halic.edu.tr>
2021-09-22 21:52:12 +0300

    70

--- MvcProjectCamp/Controllers/LoginController.cs
+++ MvcProjectCamp/Controllers/LoginController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
 
 namespace MvcProjectCamp.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        } 
+        [HttpPost]
+        public ActionResult Index(Admin a)
+        {
+            Context c = new Context();
+            var adminuserInfo = c.Admins.FirstOrDefault(x =>
+                x.AdminUserName == a.AdminUserName && x.AdminPassword == a.AdminPassword);
+            if (adminuserInfo != null )
+            {
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                return RedirectToAction("Page404","ErrorPages");
+
+            }
+
             return View();
         }
     }
