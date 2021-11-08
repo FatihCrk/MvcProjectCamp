ff47fc3c8b213e71b927784dcdf16a49f3bbde8e
Fatih ÇIRAK <fatihcirak@halic.edu.tr>
2021-11-08 11:22:35 +0300

    test

--- MvcProjectCamp/Controllers/WriterPanelController.cs
+++ MvcProjectCamp/Controllers/WriterPanelController.cs
@@ -5,12 +5,15 @@ using System.Web;
 using System.Web.Mvc;
 using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 
 namespace MvcProjectCamp.Controllers
 {
     public class WriterPanelController : Controller
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+
+        private CategoryManager cm = new CategoryManager(new EfCategoryDal());
         // GET: WriterPanel
         public ActionResult WriterProfile()
         {
@@ -19,9 +22,37 @@ namespace MvcProjectCamp.Controllers
 
         public ActionResult MyHeading()
         {
-       
+
             var myHeadingValues = hm.GetListByWriter();
             return View(myHeadingValues);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+
+            List<SelectListItem> valueCategory = (from c in cm.GetCategoryList() //Listeleme 
+                                                  select new SelectListItem()
+                                                  {
+                                                      Text = c.CategoryName,
+                                                      Value = c.CategoryId.ToString()
+
+                                                  }).ToList();
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAddBl(p);
+
+
+            return View("MyHeading");
+
         }
     }
 }
