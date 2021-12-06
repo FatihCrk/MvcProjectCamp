26d95aee2787c7d7209914a641da36ccb999d4bc
Fatih ÇIRAK <fatihcirak@halic.edu.tr>
2021-12-06 19:07:43 +0300

    80/2

---
+++ MvcProjectCamp/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        public Action Headings() 
+        { 
+            return View; }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
